1287debb92fb87c7dfdecb5774fe0b523ec6ed29
AdritLib <vcamposmiguel@gmail.com>
2026-02-20 15:06:05 -0500

    First commit

---
+++ MovieReservationBackend/src/main/java/com/moviereservation/MovieReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moviereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReservationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReservationBackendApplication.class, args);
+	}
+
+}
